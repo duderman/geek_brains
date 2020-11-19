66976263c755025e2ed7a1846d2b6ab0fcd60d6d
Nikolay Norkin <nikolay.norkin@forwardpmx.com>
2020-11-19 19:35:27 +0000

    4

--- python/3/utils.py
+++ python/3/utils.py
@@ -1,6 +1,24 @@
-def ask_int(what):
+DEFAULT_ERROR_MESSAGE = 'Wrong number'
+
+
+def ask_int(what, check=None, msg=None):
+  if msg is None:
+    msg = DEFAULT_ERROR_MESSAGE
   while True:
     try:
-      return int(input(f"{what}: "))
+      n = int(input(f"{what}: "))
+      if __perform_check(n, check, msg):
+        return n
     except ValueError:
       print("Not a number")
+
+
+def __perform_check(n, check, msg):
+  if check is None:
+    return True
+  else:
+    if check(n):
+      return True
+    else:
+      print(msg)
+      return False
