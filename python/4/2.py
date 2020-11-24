c4bb4fc5df262d5ed7d343c94d8033bcc5bb5b0e
Nikolay Norkin <nikolay.norkin@forwardpmx.com>
2020-11-24 18:17:26 +0000

    4

--- python/4/2.py
+++ python/4/2.py
@@ -3,22 +3,9 @@
 # Пример исходного списка: [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55].
 # Результат: [12, 44, 4, 10, 78, 123].
 
-import random
+from utils import generate_random_list
 
-
-def generate_random_list(length):
-  if length < 1:
-    raise ValueError('length must be greater than 0')
-
-  list = []
-  for _ in range(length):
-    n = random.randint(1, 1000)
-    list.append(n)
-
-  return list
-
-
-list = generate_random_list(10)
+list = generate_random_list()
 lower_list = [list[i] for i in range(1, len(list)) if list[i] < list[i - 1]]
 
 print(f"Original list: {list}")
